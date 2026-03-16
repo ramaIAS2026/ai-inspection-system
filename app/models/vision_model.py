7ea9ac866a9974d0d952d8a5506b1fc898da7680
PuppalaS <puppalas@PuppalaSs-MacBook-Air.local>
2026-03-16 22:11:37 +0530

    final project version

--- app/models/vision_model.py
+++ app/models/vision_model.py
@@ -1,13 +1,22 @@
 from langchain_openai import ChatOpenAI
-from app.config.settings import MODEL_NAME
-
-from langchain_openai import ChatOpenAI
+import os
 
 def get_llm():
     return ChatOpenAI(
         model="gpt-4o-mini",
         temperature=0
     )
+
+# from langchain_openai import ChatOpenAI
+# from app.config.settings import MODEL_NAME
+
+# from langchain_openai import ChatOpenAI
+
+# def get_llm():
+#     return ChatOpenAI(
+#         model="gpt-4o-mini",
+#         temperature=0
+#     )
 
 #def get_llm():
 
